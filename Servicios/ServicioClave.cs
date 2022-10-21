a26b904c30e77cd686b1e29d2eb4b6a73893f9a5
ccastova <ccastrova@choucairtesting.com>
2022-10-21 18:28:41 -0500

    TDD

---
+++ Servicios/ServicioClave.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Servicios
+{
+	public class ServicioClave
+	{
+		public bool ValidarClave(string clave) {
+			if (ValidarMayuscula(clave) && ValidarMinuscula(clave) && ValidarLongitudClave(clave) && ValidarDigito(clave) && ValidarCaracterEspecialClave(clave)) return true;
+			return false;
+		}
+
+		public bool ValidarMayuscula(string clave) {
+
+			foreach (char ch in clave) {
+				if (Char.IsUpper(ch)) {
+					return true;
+				}
+			}
+			return false;
+		}
+
+		public bool ValidarMinuscula(string clave)
+		{
+			foreach (char ch in clave)
+			{
+				if (Char.IsLower(ch))
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+
+		public bool ValidarLongitudClave(string clave)
+		{
+			if (clave.Length >= 6 && clave.Length <= 12) {
+				return true;
+			}
+			return false;
+		}
+
+		public bool ValidarCaracterEspecialClave(string clave)
+		{
+			var regexCaracterEspecial = new Regex(@"[~!#$%&/()=°<>|_{}+*.,:;@?¡¿'-]");
+			if (!regexCaracterEspecial.IsMatch(clave)) {
+				return false;
+			}
+			return true;
+		}
+
+		public bool ValidarDigito(string clave)
+		{
+			foreach (var ch in clave)
+			{
+				if (Char.IsDigit(ch))
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+}
---
+++ Test/ClaveTest.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+using Servicios;
+
+namespace Test
+{
+	/*
+	 * La clave debe tener:
+	 * -Al menos 1 mayuscula
+	 * -Al menos 1 minuscula
+	 * -6 caracteres como minimo
+	 * -12 caracteres como maximo
+	 * -Al menos 1 caracter especial (@/_-)
+	 * -Al menos 1 digito
+	 */
+	class ClaveTest
+	{
+		ServicioClave claveServicio;
+		[SetUp]
+		public void SetUp() {
+			ServicioClave claveServicio = new ServicioClave();
+		}
+
+		[TestCase("1234@", false)]
+		[TestCase("12345678912ASZzx@_", false)]
+		[TestCase("clave123", false)]
+		[TestCase("clave123_x", false)]
+		[TestCase("Clave123_X", true)]
+		public void Validar_Composicion_Clave(string clave, bool esperado) 
+		{			
+			//When
+			bool actual = claveServicio.ValidarClave(clave);
+			//Then
+			Assert.AreEqual(actual, esperado);
+		}
+
+		[TestCase("Axz19",true)]
+		[TestCase("axz19", false)]
+		[TestCase("jsdajxFsda", true)]
+		[TestCase("123adaA", true)]
+		public void Calve_Debe_Tener_Almenos_1_Mayuscula(string clave, bool esperado) 
+		{
+			//When
+			bool actual = claveServicio.ValidarMayuscula(clave);
+			//Then
+			Assert.AreEqual(actual, esperado);
+		}
+
+		[TestCase("X90A123", false)]
+		[TestCase("AAAAAAAAA", false)]
+		[TestCase("jsdajx233AASX", true)]
+		[TestCase("aXXXXXa", true)]
+		[TestCase("12345AB", false)]
+		public void Clave_Debe_Tener_Almenos_1_Minuscula(string clave, bool esperado) 
+		{
+			//When
+			bool resultado = claveServicio.ValidarMinuscula(clave);
+			//Then
+			Assert.AreEqual(resultado, esperado);
+		}
+
+		[TestCase("123", false)]
+		[TestCase("123456", true)]
+		[TestCase("80902", false)]
+		[TestCase("xMA321", true)]
+		[TestCase("POyx9012er81", true)]
+		[TestCase("000000eeaattMAXMIN", false)]
+		public void Clave_Debe_Tener_Longitud_Maxima_Entre_16_y_12Caracteres(string clave, bool esperado)
+		{
+			//When
+			bool resultado = claveServicio.ValidarLongitudClave(clave);
+			//Then
+			Assert.AreEqual(resultado, esperado);
+		}
+
+		[TestCase("Mxz@pop12", true)]
+		[TestCase("Maxsdd1233", false)]
+		[TestCase("as_@ssii1", true)]
+		[TestCase("POPASSD", false)]
+		public void Clave_Debe_Tener_Almenos_1_Caractere_Especial(string clave, bool esperado)
+		{
+			//When
+			bool resultado = claveServicio.ValidarCaracterEspecialClave(clave);
+			//Then
+			Assert.AreEqual(esperado, resultado);
+		}
+
+		[TestCase("12313129", true)]
+		[TestCase("axcaASD@_", false)]
+		[TestCase("ipascqweeAXCA@_2", true)]
+		[TestCase("AAXCpioxASD21", true)]
+		public void Clave_Debe_Tener_Almenos_1_Digito(string clave, bool esperado)
+		{
+			//When
+			bool resultado = claveServicio.ValidarDigito(clave);
+			//Then
+			Assert.AreEqual(esperado, resultado);
+		}
+	}
+}
